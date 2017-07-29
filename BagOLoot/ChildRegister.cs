ca02e8e0258faad113c1cfa56e0ac1cca7620bfd
Nathan Gonzalez <nathan@copypastedeveloper.com>
2017-07-29 11:57:43 -0500

    Update ChildRegister.cs

--- BagOLoot/ChildRegister.cs
+++ BagOLoot/ChildRegister.cs
@@ -8,18 +8,8 @@ namespace BagOLoot
     public class ChildRegister
     {
         private List<Child> _children = new List<Child>();
-        private DatabaseInterface _db;
-
-        public ChildRegister(DatabaseInterface db)
-        {
-            _db = db;
-        }
-
         public int AddChild (string child) 
         {
-            int id = _db.Insert( $"insert into child values (null, '{child}', 0)");
-
-
             _children.Add(
                 new Child()
                 {
@@ -32,26 +22,9 @@ namespace BagOLoot
             return id;
         }
 
-        public List<Child> GetChildren ()
-        {
-            _db.Query("select id, name, delivered from child",
-                (SqliteDataReader reader) => {
-                    _children.Clear();
-                    while (reader.Read ())
-                    {
-                        _children.Add(new Child(){
-                            id = reader.GetInt32(0),
-                            name = reader[1].ToString(),
-                            delivered = reader.GetInt32(2) == 1
-                        });
-                    }
-                }
-            );
-
-            return _children;
-        }
+        public List<Child> GetChildren () => _children;
 
         public Child GetChild (int id) =>  _children.SingleOrDefault(kid => kid.id == id);
-
+        
     }
 }
